ca91de3d2fa4e4ccd0a2c1cad5be1e17551c3e46
sarojchin <sarojchin@gmail.com>
2017-10-16 15:28:18 -0700

    Update GoodAuction.sol

--- contracts/GoodAuction.sol
+++ contracts/GoodAuction.sol
@@ -2,34 +2,43 @@ pragma solidity ^0.4.15;
 
 import "./AuctionInterface.sol";
 
-/** @title BadAuction */
-contract BadAuction is AuctionInterface {
-	/* Bid function, vulnerable to attack
-	 * Must return true on successful send and/or bid,
-	 * bidder reassignment
-	 * Must return false on failure and send people
-	 * their funds back
+/** @title GoodAuction */
+contract GoodAuction is AuctionInterface {
+	/* New data structure, keeps track of refunds owed to ex-highest bidders */
+	mapping(address => uint) refunds;
+
+	/* Bid function, shifts to push paradigm
+	 * Must return true on successful send and/or bid, bidder
+	 * reassignment
+	 * Must return false on failure and allow people to
+	 * retrieve their funds
 	 */
-	 
-	//modifier hasEnough() { require(msg.sender.send(msg.value)); _;}
-	function bid() payable external returns (bool) {
+	function bid() payable external returns(bool) {
 			if (msg.value <= highestBid) {
-				if (!msg.sender.send(msg.value)) {
-					throw;
-				}
-			return false;
+				refunds[msg.sender] = msg.value;
+				return false;
 		}
 
-		if (!highestBidder.send(highestBid)) {
-			if (!msg.sender.send(msg.value)) {
-				throw;
-			}
-			return false;
-		}
+		refunds[highestBidder] = highestBid;
 
 		highestBidder = msg.sender;
 		highestBid = msg.value;
+	}
 
+	/* New withdraw function, shifts to push paradigm */
+	function withdrawRefund() external returns(bool) {
+		if (refunds[msg.sender] == 0) {
+			return false;
+	  }
+		uint toTransfer = refunds[msg.sender];
+	    refunds[msg.sender] = 0;
+	    msg.sender.transfer(toTransfer);
+		return true;
+	}
+
+	/* Allow users to check the amount they can withdraw */
+	function getMyBalance() constant external returns(uint) {
+		return refunds[msg.sender];
 	}
 
 	/* Give people their funds back */
