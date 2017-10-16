3f137305745e611e3e5a8f5330371a92109cb7c4
sarojchin <sarojchin@gmail.com>
2017-10-16 15:28:32 -0700

    Update BadAuction.sol

--- contracts/BadAuction.sol
+++ contracts/BadAuction.sol
@@ -10,12 +10,30 @@ contract BadAuction is AuctionInterface {
 	 * Must return false on failure and send people
 	 * their funds back
 	 */
+	 
+	//modifier hasEnough() { require(msg.sender.send(msg.value)); _;}
 	function bid() payable external returns (bool) {
-		// YOUR CODE HERE
+			if (msg.value <= highestBid) {
+				if (!msg.sender.send(msg.value)) {
+					throw;
+				}
+			return false;
+		}
+
+		if (!highestBidder.send(highestBid)) {
+			if (!msg.sender.send(msg.value)) {
+				throw;
+			}
+			return false;
+		}
+
+		highestBidder = msg.sender;
+		highestBid = msg.value;
+
 	}
 
 	/* Give people their funds back */
 	function () payable {
-		// YOUR CODE HERE
+		msg.sender.transfer(msg.value);
 	}
 }
